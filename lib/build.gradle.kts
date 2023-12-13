1cde9417ce9f5fa4f8e721bd5fe36516d3daf4cd
Jan Seeger <seeger@sipgate.de>
2023-12-13 18:26:18 +0100

    Ready library for publication

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,20 +1,15 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.kotlin.serialization)
+    `maven-publish`
+    signing
 }
 
 android {
     namespace = "de.sipgate.federmappe"
     compileSdk = 34
-
-    defaultConfig {
-        applicationId = "de.sipgate.federmappe"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
+    defaultConfig.minSdk = 24
 
     buildTypes {
         release {
@@ -36,4 +31,40 @@ dependencies {
     implementation(libs.firebase.firestore)
 
     testImplementation(libs.junit)
+}
+
+publishing {
+    publications.withType<MavenPublication> {
+        groupId = "de.sipgate"
+        artifactId = project.name
+        version = project.version.toString()
+
+        pom {
+            name.set("Federmappe")
+            description.set("")
+            url.set("https://github.com/sipgate/federmappe")
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://git@github.com:sipgate/federmappe.git")
+                developerConnection.set("scm:git:ssh://git@github.com:sipgate/federmappe.git")
+                url.set("https://github.com/sipgate/federmappe")
+            }
+        }
+
+        afterEvaluate {
+            from(components["release"])
+        }
+    }
+}
+
+signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications)
 }
