22e03e10510238ddf07b6ded506459176eaffda6
Jan Seeger <seeger@sipgate.de>
2024-02-13 13:55:38 +0100

    Provide the option of passing custom Serializers

--- lib/src/main/java/de/sipgate/federmappe/StringMapExt.kt
+++ lib/src/main/java/de/sipgate/federmappe/StringMapExt.kt
@@ -6,11 +6,12 @@ import kotlinx.serialization.modules.contextual
 import kotlinx.serialization.serializer
 
 inline fun <reified T : Any> Map<String, Any>.toObjectWithSerializer(
-    serializer: KSerializer<T> = serializer<T>()
+    serializer: KSerializer<T> = serializer<T>(),
+    customSerializers: SerializersModule = SerializersModule { contextual(DateSerializer) }
 ): T = serializer.deserialize(
     StringMapToObjectDecoder(
         this,
         ignoreUnknownProperties = true,
-        serializersModule = SerializersModule { contextual(DateSerializer) },
+        serializersModule = customSerializers,
     ),
 )
