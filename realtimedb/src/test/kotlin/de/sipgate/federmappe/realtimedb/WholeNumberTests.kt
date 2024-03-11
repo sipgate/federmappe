30b8f70cb81eeac015f980da8ccc481831559d5c
Jan Seeger <seeger@sipgate.de>
2024-03-11 16:49:45 +0100

    Add long decoding test

--- realtimedb/src/test/kotlin/de/sipgate/federmappe/realtimedb/WholeNumberTests.kt
+++ realtimedb/src/test/kotlin/de/sipgate/federmappe/realtimedb/WholeNumberTests.kt
@@ -31,4 +31,28 @@ class WholeNumberTests {
         Assertions.assertNotNull(output)
         Assertions.assertEquals(1234, output.someValue)
     }
+
+    @Test
+    fun longDecodingTest() {
+        @Serializable
+        data class Asdf(val someValue: Long)
+
+        val expectedProperty = mockk<DataSnapshot> {
+            every { value } returns 1234L
+            every { key } returns "someValue"
+            every { children } returns emptyList()
+        }
+
+        val data = mockk<DataSnapshot> {
+            every { key } returns null
+            every { value } returns null
+            every { children } returns listOf(expectedProperty)
+            every { child("someValue") } returns expectedProperty
+        }
+
+        val output = data.toObjectWithSerializer(Asdf.serializer())
+
+        Assertions.assertNotNull(output)
+        Assertions.assertEquals(1234L, output.someValue)
+    }
 }
