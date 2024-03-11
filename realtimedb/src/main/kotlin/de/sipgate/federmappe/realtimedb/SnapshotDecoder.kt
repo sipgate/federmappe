53f8c72cec1a0691a7232f2853f70142000b6574
Jan Seeger <seeger@sipgate.de>
2024-03-11 16:16:02 +0100

    Simple String property decoding

--- realtimedb/src/main/kotlin/de/sipgate/federmappe/realtimedb/SnapshotDecoder.kt
+++ realtimedb/src/main/kotlin/de/sipgate/federmappe/realtimedb/SnapshotDecoder.kt
@@ -1,18 +1,53 @@
 package de.sipgate.federmappe.realtimedb
 
 import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.getValue
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.StructureKind
 import kotlinx.serialization.encoding.AbstractDecoder
 import kotlinx.serialization.encoding.CompositeDecoder
 import kotlinx.serialization.modules.SerializersModule
 
+@ExperimentalSerializationApi
 class SnapshotDecoder(
     private val dataSnapshot: DataSnapshot,
     private val ignoreUnknownProperties: Boolean,
     override val serializersModule: SerializersModule
 ) : AbstractDecoder() {
 
+    private val keysIterator = dataSnapshot.children.mapNotNull { it.key }.iterator()
+    private var index: Int? = null
+    var key: String? = null
+
+    private val skippedValues = mutableSetOf<String>()
+
+    override fun decodeSequentially() = false
+
+    override fun decodeCollectionSize(descriptor: SerialDescriptor): Int = dataSnapshot.children.count()
+
     override fun decodeElementIndex(descriptor: SerialDescriptor): Int {
+        while (keysIterator.hasNext()) {
+            val nextKey = keysIterator.next().also { key = it }
+
+            val nextIndex =
+                when (descriptor.kind) {
+//                    StructureKind.MAP -> dataSnapshot.children.indexOf(nextKey)
+                    else -> descriptor.getElementIndex(nextKey)
+                }
+
+            if (nextIndex == CompositeDecoder.UNKNOWN_NAME) {
+                skippedValues.add(nextKey)
+                continue
+            }
+
+            return nextIndex.also { index = it }
+        }
+
         return CompositeDecoder.DECODE_DONE
+    }
+
+    override fun decodeValue(): Any {
+        return dataSnapshot.child(key!!).value!!
     }
 }
