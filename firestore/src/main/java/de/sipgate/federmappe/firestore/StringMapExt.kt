d7f9830a1d3bb5e79e1a40eec281cf91fd406ba5
Jan Seeger <seeger@sipgate.de>
2024-03-12 11:30:04 +0100

    Add missing Annotation

--- firestore/src/main/java/de/sipgate/federmappe/firestore/StringMapExt.kt
+++ firestore/src/main/java/de/sipgate/federmappe/firestore/StringMapExt.kt
@@ -1,9 +1,11 @@
 package de.sipgate.federmappe.firestore
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.serializer
 
+@ExperimentalSerializationApi
 inline fun <reified T : Any> Map<String, Any>.toObjectWithSerializer(
     serializer: KSerializer<T> = serializer<T>(),
     customSerializers: SerializersModule
