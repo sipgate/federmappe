3472e9800c03a2c904f0feb99f5ee634303b5630
Jan Seeger <seeger@sipgate.de>
2024-03-12 10:51:31 +0100

    Add nested structures decoding test

---
+++ firestore/src/test/java/de/sipgate/federmappe/firestore/SubclassDecodingTests.kt
@@ -0,0 +1,40 @@
+package de.sipgate.federmappe.firestore
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.serializer
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertInstanceOf
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalSerializationApi::class)
+class SubclassDecodingTests {
+    @Serializable
+    data class A(val b: String)
+
+    @Test
+    fun deserializeNestedDataClass() {
+        // Arrange
+        @Serializable
+        data class TestClass(val a: A)
+
+        val serializer = serializer<TestClass>()
+        val data = mapOf<String, Any?>("a" to mapOf("b" to "Some String"))
+
+        // Act
+        val result =
+            serializer.deserialize(
+                StringMapToObjectDecoder(
+                    data,
+                    ignoreUnknownProperties = true,
+                ),
+            )
+
+        // Assert
+        assertInstanceOf(TestClass::class.java, result)
+        assertNotNull(result.a)
+        assertEquals("Some String", result.a.b)
+    }
+}
