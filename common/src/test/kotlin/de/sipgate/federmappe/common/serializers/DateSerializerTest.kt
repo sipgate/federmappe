e619393b718a699066c15271bf127d5b3316fdd2
Jan Seeger <seeger@sipgate.de>
2024-03-20 17:19:49 +0100

    Have DateSerializer support sequential and arbitrary ordered decoding

---
+++ common/src/test/kotlin/de/sipgate/federmappe/common/serializers/DateSerializerTest.kt
@@ -0,0 +1,39 @@
+package de.sipgate.federmappe.common.serializers
+
+import de.sipgate.federmappe.common.StringMapToObjectDecoder
+import java.util.Date
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.serializer
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+
+class DateSerializerTest {
+    private val epochSeconds = 1707833611L * 1000
+    private val nanos = 801 / 1000000
+
+    /* Nanos will be capped at millisecond precision, because java.util.Date isn't precise enough. */
+    private val date = Date(epochSeconds + nanos)
+
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun testDeserialization() {
+        @Serializable
+        data class TestClass(val a: @Serializable(with = DateSerializer::class) Date)
+
+        val serializer = serializer<TestClass>()
+        val data = mapOf<String, Any?>(
+            "a" to mapOf(
+                "seconds" to 1707833611L,
+                "nanoseconds" to 801L
+            )
+        )
+
+        val result = serializer.deserialize(StringMapToObjectDecoder(data))
+
+        // Assert
+        Assertions.assertEquals(date, result.a)
+        Assertions.assertInstanceOf(TestClass::class.java, result)
+    }
+}
