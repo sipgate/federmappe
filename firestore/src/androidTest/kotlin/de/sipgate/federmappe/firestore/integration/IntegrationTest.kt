60646dd9b914b824ccf8853a2a6b095d93d5b26f
Jan Seeger <seeger@sipgate.de>
2024-05-28 17:51:10 +0200

    Clean up test init

--- firestore/src/androidTest/kotlin/de/sipgate/federmappe/firestore/integration/IntegrationTest.kt
+++ firestore/src/androidTest/kotlin/de/sipgate/federmappe/firestore/integration/IntegrationTest.kt
@@ -2,12 +2,12 @@ package de.sipgate.federmappe.firestore.integration
 
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseOptions
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.ktx.initialize
-import kotlinx.coroutines.tasks.await
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.initialize
 import de.sipgate.federmappe.firestore.toObjects
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.runTest
 import kotlinx.datetime.Instant
 import kotlinx.datetime.serializers.InstantComponentSerializer
@@ -15,29 +15,30 @@ import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
 import org.junit.Test
 
+@OptIn(ExperimentalSerializationApi::class)
 class IntegrationTest {
 
-    init {
-        val firebaseApp = Firebase.initialize(
-            ApplicationProvider.getApplicationContext<Context>(),
-            options = FirebaseOptions.Builder().setApplicationId("demo-test")
-                .setProjectId("demo-test").build()
-        )
-        Firebase.firestore(firebaseApp)
-            .useEmulator("10.0.2.2", 8080)
+    private val firebaseApp = Firebase.initialize(
+        ApplicationProvider.getApplicationContext<Context>(),
+        options = FirebaseOptions.Builder()
+            .setApplicationId("demo-test")
+            .setProjectId("demo-test")
+            .build()
+    )
+    private val firestore = FirebaseFirestore.getInstance(firebaseApp).apply {
+        useEmulator("10.0.2.2", 8080)
     }
 
-    @Serializable
-    data class User(
-        val id: String,
-        @Serializable(with = InstantComponentSerializer::class)
-        val createdAt: Instant,
-    )
-
-    @OptIn(ExperimentalSerializationApi::class)
     @Test
     fun asdf(): Unit = runTest {
-        val a = Firebase.firestore.collection("users").get().await().toObjects<User>()
+        @Serializable
+        data class User(
+            val id: String,
+            @Serializable(with = InstantComponentSerializer::class)
+            val createdAt: Instant,
+        )
+
+        val a = firestore.collection("users").get().await().toObjects<User>()
         a
     }
 }
